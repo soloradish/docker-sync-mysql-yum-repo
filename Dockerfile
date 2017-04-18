b7035092915fa89b8792e77f5940a0be8183a6c6
LowID <soloradish@gmail.com>
2017-04-18 16:21:22 +0800

    use temp dir

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@ FROM centos:7
 
 RUN yum -y install epel-release \
  && yum -y install reposync createrepo \
- && yum --enablerepo=* clean all 
+ && yum --enablerepo=* clean all \
+ && rm -rf /var/cache/yum \
+ && rm -f /etc/yum.repos.d/*.repo
 
 COPY mysql-56-repo.conf /opt/
 COPY syncrepo.sh /opt/
--- syncrepo.sh
+++ syncrepo.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+mkdir -p /tmp/mysql
 mkdir -p /repos/mysql
-reposync -n -c /opt/mysql-56-repo.conf -p /repos/mysql -d -r mysql56-community -r mysql-connectors-community -r mysql-tools-community
+
+reposync -n -c /opt/mysql-56-repo.conf -p /tmp/mysql -d -r mysql56-community -r mysql-connectors-community -r mysql-tools-community
 createrepo /repos/mysql/mysql56-community
 createrepo /repos/mysql/mysql-connectors-community
 createrepo /repos/mysql/mysql-tools-community
+
+cp -R /tmp/mysql/ /repos/mysql/
+rm -Rf /tmp/mysql
