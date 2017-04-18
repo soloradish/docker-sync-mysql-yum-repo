440aff7502d855e66be479b2ab4ed3d43f3c6955
LowID <soloradish@gmail.com>
2017-04-18 11:33:04 +0800

    init commit

---
+++ syncrepo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+reposync -n -c /opt/mysql-56-repo.conf -p /repos/mysql -d -r mysql56-community -r mysql-connectors-community -r mysql-tools-community
+createrepo /repos/mysql/mysql56-community
+createrepo /repos/mysql/mysql-connectors-community
+createrepo /repos/mysql/mysql-tools-community
