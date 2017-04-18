27058e2f407d271d2c5d2ab210a91f9b91105013
LowID <soloradish@gmail.com>
2017-04-18 16:28:21 +0800

    reorder sh

--- syncrepo.sh
+++ syncrepo.sh
@@ -4,9 +4,10 @@ mkdir -p /tmp/mysql
 mkdir -p /repos/mysql
 
 reposync -n -c /opt/mysql-56-repo.conf -p /tmp/mysql -d -r mysql56-community -r mysql-connectors-community -r mysql-tools-community
-createrepo /repos/mysql/mysql56-community
-createrepo /repos/mysql/mysql-connectors-community
-createrepo /repos/mysql/mysql-tools-community
 
 cp -R /tmp/mysql/ /repos/mysql/
 rm -Rf /tmp/mysql
+
+createrepo /repos/mysql/mysql56-community
+createrepo /repos/mysql/mysql-connectors-community
+createrepo /repos/mysql/mysql-tools-community
