6f9dbb077c860e2fe5bf291b0d4f2320289357ff
LowID <soloradish@gmail.com>
2017-04-18 16:29:45 +0800

    fix tmp dir

--- syncrepo.sh
+++ syncrepo.sh
@@ -5,9 +5,10 @@ mkdir -p /repos/mysql
 
 reposync -n -c /opt/mysql-56-repo.conf -p /tmp/mysql -d -r mysql56-community -r mysql-connectors-community -r mysql-tools-community
 
+createrepo /tmp/mysql/mysql56-community
+createrepo /tmp/mysql/mysql-connectors-community
+createrepo /tmp/mysql/mysql-tools-community
+
 cp -R /tmp/mysql/ /repos/mysql/
 rm -Rf /tmp/mysql
 
-createrepo /repos/mysql/mysql56-community
-createrepo /repos/mysql/mysql-connectors-community
-createrepo /repos/mysql/mysql-tools-community
